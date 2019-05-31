97b7dd88d585cb35bcb4fc29e1fc91eeefd74a4a
Artur Khayrullin <kehel@yandex.ru>
2019-05-31 12:35:09 +0300

    GTGD FPS p3

--- GTGD/FPS/Assets/Project/Scripts/Player.cs
+++ GTGD/FPS/Assets/Project/Scripts/Player.cs
@@ -2,13 +2,28 @@
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private float _bulletForce;
+    [SerializeField] private float _timeForCharge;
+
+    private float _timeForShot;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButton("Fire1") && _timeForShot<=0) {
+            _timeForShot = _timeForCharge;
+            InstantiateBullet();
+        }
+        _timeForShot -= Time.deltaTime;
 	}
+
+    private void InstantiateBullet() {
+        var bullet = Instantiate(_bulletPrefab, transform.TransformPoint(0, 0, 0.5f), transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * _bulletForce, ForceMode.Impulse);
+    }
 }
