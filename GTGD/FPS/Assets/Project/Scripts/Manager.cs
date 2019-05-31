0be876fc52a12500dbb36472461c4bfeae10eed7
Artur Khayrullin <kehel@yandex.ru>
2019-05-31 12:14:42 +0300

    GTGD FPS p2

--- GTGD/FPS/Assets/Project/Scripts/Manager.cs
+++ GTGD/FPS/Assets/Project/Scripts/Manager.cs
@@ -1,9 +1,13 @@
 ﻿using Base;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour {
 
     public event GameEvent GameEvent;
+    [SerializeField] private GameObject _messagePanel;
+    [SerializeField] private Text _messageText;
 
     public static Manager Instance;
 
@@ -25,7 +29,7 @@ public class Manager : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
+        ShowMessage("Hello there! There is a treasure chest right on this level! So be quick and gold will stick!");
     }
 
     // Update is called once per frame
@@ -37,5 +41,23 @@ public class Manager : MonoBehaviour {
         Debug.Log("ProcessGameEvent");
         if (GameEvent != null)
             GameEvent();
+    }
+
+    public void ShowMessage(string message) {
+        StartCoroutine(ShowMessageRoutine(message));
+    }
+
+    private IEnumerator ShowMessageRoutine(string message) {
+        _messageText.text = message;
+        _messagePanel.SetActive(true);
+        yield return new WaitForSeconds(2);
+
+        var image = _messagePanel.GetComponent<Image>();
+        for (int i = 1; i <= 100; i++) {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, (100.0f - i) / 200);
+            yield return new WaitForSeconds(0.01f);
+        }
+        _messagePanel.SetActive(false);
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
     }
 }
