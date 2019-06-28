c8dc6d1ff2867c52196732e3a252a0686771585c
Artur Khayrullin <kehel@yandex.ru>
2019-06-28 18:50:03 +0300

    Update TileVania project

---
+++ Udemy_Complete_C#_Unity_Developer_2D/11_TileVania_2019.1.5f1/Assets/Project/Scripts/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
